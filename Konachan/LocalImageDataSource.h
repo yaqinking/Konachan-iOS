74660ca56a2fb03d4305341d87a8edf8dcef0583
小笠原やきん <yaqinking@yahoo.co.jp>
2016-05-13 12:36:46 +0800

    Update Pods

---
+++ Konachan/LocalImageDataSource.h
@@ -0,0 +1,13 @@
+//
+//  LocalImageDataSource.h
+//  Konachan
+//
+//  Created by 小笠原やきん on 16/5/13.
+//  Copyright © 2016年 yaqinking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalImageDataSource : NSObject
+
+@end
---
+++ Konachan/LocalImageDataSource.m
@@ -0,0 +1,13 @@
+//
+//  LocalImageDataSource.m
+//  Konachan
+//
+//  Created by 小笠原やきん on 16/5/13.
+//  Copyright © 2016年 yaqinking. All rights reserved.
+//
+
+#import "LocalImageDataSource.h"
+
+@implementation LocalImageDataSource
+
+@end
